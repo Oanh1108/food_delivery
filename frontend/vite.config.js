a5770b48ea14078a1dd03b8edfd1b8f49cdf12b3
Oanh1108 <kieuoanh10824@gmail.com>
2025-11-08 16:09:15 +0700

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/food_delivery"
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    outDir: 'build'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
